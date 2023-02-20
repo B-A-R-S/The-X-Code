bc1bf09e88af4be6536dbec67aa2e98f264fcbf4
Roshina Shemashi Perera <108880796+B-A-R-S@users.noreply.github.com>
2023-02-20 15:02:53 +0000

    Delete InsulinRecommendation.py

--- InsulinRecommendation.py
+++
@@ -1,7 +0,0 @@
-
-# Evaluate the algorithm
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squarred Error:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Mean Squarred Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-score = r2_score(y_test, y_pred)
-print('r2 score:', score)
