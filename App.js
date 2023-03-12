bc96b82c8967ed846035f9d5e02ab4b343895654
Ruwandieka Peiris <93978727+rpasbamboo@users.noreply.github.com>
2023-03-12 03:15:10 -0700

    Update App.js
    modified app.js

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-//import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, Button, Platform, StatusBar} from 'react-native';
-import SelectAccount from "./src/SelectAccount";
-import WorkoutRec from "./src/WorkoutRec";
-import Home from "./src/Home";
-import {useDimensions } from '@react-native-community/hooks';
-import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import PrivacyPolicyScreen from './screens/PrivacyPolicyScreen';
+import MealRecommendation from './screens/MealRecommendationScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="About Us" component={AboutScreen} />
+      <Stack.Screen name="Privacy Policy" component={PrivacyPolicyScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function MainTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Meal Recommendations" component={MealRecommendation} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
-  return<WorkoutRec/>
+  return (
+    <NavigationContainer>
+      <MainTab />
+    </NavigationContainer>
+  );
 }
